6201d5484925d376da92a83c13c2651b7c26b55b
İlkay Günel <ilkay.gunel@kod5.org>
2015-06-22 22:10:24 +0300

    Sql Dosyası Güncellendi

--- HospitalAutomation.sql
+++ HospitalAutomation.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 14, 2015 at 08:47 AM
+-- Generation Time: Jun 22, 2015 at 09:09 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -168,6 +168,28 @@ INSERT INTO `klinikyerleri` (`id`, `hastaneid`, `klinikyeri`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `patient`
+--
+
+CREATE TABLE IF NOT EXISTS `patient` (
+  `identitynumber` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `birthdate` date DEFAULT NULL,
+  `birthplace` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `emailaddress` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT '0',
+  `fathername` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `gender` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `mothername` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `phonenumber` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `role` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `surname` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `patients`
 --
 
@@ -192,7 +214,8 @@ CREATE TABLE IF NOT EXISTS `patients` (
 --
 
 INSERT INTO `patients` (`identitynumber`, `birthdate`, `birthplace`, `emailaddress`, `enabled`, `fathername`, `gender`, `mothername`, `name`, `password`, `phonenumber`, `role`, `surname`) VALUES
-('11111111111', '1993-01-01', 'Samsun', 'abc@abc.com', 1, 'Üzeyir', 'Erkek', 'Fatma', 'İlkay', '827ccb0eea8a706c4c34a16891f84e7b', '0(511) 111-1111', 'ROLE_GUEST', 'Günel');
+('11111111111', '1993-01-01', 'Samsun', 'abc@abc.com', 1, 'Üzeyir', 'Erkek', 'Fatma', 'İlkay', '827ccb0eea8a706c4c34a16891f84e7b', '0(511) 111-1111', 'ROLE_GUEST', 'Günel'),
+('11111111112', '0020-10-11', 'İstanbul', 'bc@bc.com', 1, 'İsmail', 'Erkek', 'Adile', 'Turgay', '827ccb0eea8a706c4c34a16891f84e7b', '0(511) 111-1111', 'ROLE_GUEST', 'Günel');
 
 --
 -- Triggers `patients`
@@ -202,6 +225,19 @@ CREATE TRIGGER `addUserRole` AFTER INSERT ON `patients`
  FOR EACH ROW INSERT INTO userroles(role,useridentitynumber) VALUES(NEW.role,NEW.identitynumber)
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persistent_logins`
+--
+
+CREATE TABLE IF NOT EXISTS `persistent_logins` (
+  `username` varchar(64) COLLATE utf8_turkish_ci NOT NULL,
+  `series` varchar(64) COLLATE utf8_turkish_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8_turkish_ci NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 -- --------------------------------------------------------
 
@@ -216,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `RANDEVUSAATLERI` (
   `SAAT` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
   `SAATALINDIMI` tinyint(1) DEFAULT '0',
   `TITLE` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
 -- Dumping data for table `RANDEVUSAATLERI`
@@ -224,10 +260,21 @@ CREATE TABLE IF NOT EXISTS `RANDEVUSAATLERI` (
 
 INSERT INTO `RANDEVUSAATLERI` (`SAATID`, `DOKTORID`, `RANDEVUID`, `SAAT`, `SAATALINDIMI`, `TITLE`) VALUES
 (1, 1, 1, '09:00', 0, ''),
-(2, 1, 1, '09:15', 1, 'DOLU'),
+(2, 1, 1, '09:15', 0, ''),
 (3, 1, 1, '09:30', 0, ''),
 (4, 1, 1, '09:45', 0, ''),
-(5, 1, 1, '10:00', 0, '');
+(5, 1, 1, '10:00', 0, ''),
+(6, 3, 3, '09:00', 0, ''),
+(7, 3, 3, '09:20', 0, ''),
+(8, 3, 3, '09:40', 0, ''),
+(9, 3, 3, '10:00', 0, ''),
+(10, 3, 3, '10:15', 0, ''),
+(11, 3, 3, '10:30', 0, ''),
+(12, 2, 4, '09:00', 0, ''),
+(13, 2, 4, '09:15', 0, ''),
+(14, 2, 4, '09:30', 0, ''),
+(15, 2, 4, '09:45', 0, ''),
+(16, 2, 4, '10:00', 0, '');
 
 -- --------------------------------------------------------
 
@@ -237,21 +284,24 @@ INSERT INTO `RANDEVUSAATLERI` (`SAATID`, `DOKTORID`, `RANDEVUID`, `SAAT`, `SAATA
 
 CREATE TABLE IF NOT EXISTS `takenappointments` (
   `takedappointmentid` int(11) NOT NULL,
+  `clockid` int(11) NOT NULL,
   `clinicname` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
   `clinicplace` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
   `date` date DEFAULT NULL,
   `doctorid` int(11) DEFAULT NULL,
   `hospitalname` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
   `hour` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
-  `patientid` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+  `patientid` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `wasappointmentcancelled` tinyint(1) NOT NULL,
+  `datepassed` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
 -- Dumping data for table `takenappointments`
 --
 
-INSERT INTO `takenappointments` (`takedappointmentid`, `clinicname`, `clinicplace`, `date`, `doctorid`, `hospitalname`, `hour`, `patientid`) VALUES
-(1, 'Genel Cerrahi', 'Merkez', '2015-06-14', 1, 'Eyüp Devlet Hastanesi', '10:00', '11111111111');
+INSERT INTO `takenappointments` (`takedappointmentid`, `clockid`, `clinicname`, `clinicplace`, `date`, `doctorid`, `hospitalname`, `hour`, `patientid`, `wasappointmentcancelled`, `datepassed`) VALUES
+(6, 15, 'Genel Cerrahi', 'Merkez', '2015-07-21', 2, 'Okmeydanı Devlet Hastanesi', '09:45', '11111111111', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -263,14 +313,15 @@ CREATE TABLE IF NOT EXISTS `userroles` (
   `id` int(11) NOT NULL,
   `role` varchar(20) COLLATE utf8_turkish_ci NOT NULL,
   `useridentitynumber` varchar(11) COLLATE utf8_turkish_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
 -- Dumping data for table `userroles`
 --
 
 INSERT INTO `userroles` (`id`, `role`, `useridentitynumber`) VALUES
-(1, 'ROLE_GUEST', '11111111111');
+(1, 'ROLE_GUEST', '11111111111'),
+(2, 'ROLE_GUEST', '11111111112');
 
 -- --------------------------------------------------------
 
@@ -287,7 +338,7 @@ CREATE TABLE IF NOT EXISTS `uygunrandevular` (
   `klinikyeri` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
   `randevualindimi` tinyint(1) DEFAULT '0',
   `tarih` date DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
 -- Dumping data for table `uygunrandevular`
@@ -296,7 +347,8 @@ CREATE TABLE IF NOT EXISTS `uygunrandevular` (
 INSERT INTO `uygunrandevular` (`uygunrandevuid`, `doktoradi`, `doktorid`, `hastaneid`, `klinikid`, `klinikyeri`, `randevualindimi`, `tarih`) VALUES
 (1, 'Hasan Karaoğlu', 1, 1, 1, 'Merkez', 0, '2015-06-14'),
 (2, 'Muhammed Murat Tutar', 2, 2, 1, 'Merkez', 0, '2015-06-14'),
-(3, 'Halil İbrahim Genç', 3, 2, 1, 'Merkez', 0, '2015-06-14');
+(3, 'Halil İbrahim Genç', 3, 2, 1, 'Merkez', 0, '2015-06-14'),
+(4, 'Muhammed Murat Tutar', 2, 2, 1, 'Merkez', 0, '2015-07-21');
 
 --
 -- Indexes for dumped tables
@@ -339,10 +391,22 @@ ALTER TABLE `klinikyerleri`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`identitynumber`);
+
+--
 -- Indexes for table `patients`
 --
 ALTER TABLE `patients`
   ADD PRIMARY KEY (`identitynumber`);
+
+--
+-- Indexes for table `persistent_logins`
+--
+ALTER TABLE `persistent_logins`
+  ADD PRIMARY KEY (`series`);
 
 --
 -- Indexes for table `RANDEVUSAATLERI`
@@ -406,22 +470,22 @@ ALTER TABLE `klinikyerleri`
 -- AUTO_INCREMENT for table `RANDEVUSAATLERI`
 --
 ALTER TABLE `RANDEVUSAATLERI`
-  MODIFY `SAATID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `SAATID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `takenappointments`
 --
 ALTER TABLE `takenappointments`
-  MODIFY `takedappointmentid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `takedappointmentid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `userroles`
 --
 ALTER TABLE `userroles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `uygunrandevular`
 --
 ALTER TABLE `uygunrandevular`
-  MODIFY `uygunrandevuid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `uygunrandevuid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
