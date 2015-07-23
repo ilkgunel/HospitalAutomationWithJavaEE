b960b5328768de6c3d5312ee3357d29ed9728e72
ilkgunel <ilkgunel93@gmail.com>
2015-07-23 10:33:21 -0400

    Telif Hakkı Sorunu Çıkabileceğinden Randevu Alma Sayfasındaki Resimler Kaldırılmıştır.

--- target/generated-sources/annotations/com/ilkgunel/hastaneotomasyonu/entity/Iller_.java
+++ target/generated-sources/annotations/com/ilkgunel/hastaneotomasyonu/entity/Iller_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-06-24T22:07:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-07-23T10:00:26")
 @StaticMetamodel(Iller.class)
 public class Iller_ { 
 
