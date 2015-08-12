a8fec8f6c5ff4bdc0e7918a193c6cd6e7ca55784
İlkay Günel <ilkgunel93@gmail.com>
2015-08-13 01:15:38 +0300

    Üye Kayıt Yöntemi Değiştirildi
    Üyeleri kayıt ederken kullanılan sistem JPA kullanımına daha uygun bir
    şekilde değiştirildi. Xhtml içindeki İlgili alanlar bir Patients entity
    nesnesinin ilgili özelliklerine atanarak metot yazıldı. Bu durumdan
    dolayı doğum tarihinde f:convertDateTime’a gerek kalmadı ve kaldırıldı.

--- src/main/java/com/ilkgunel/hastaneotomasyonu/controller/SavePatients.java
+++ src/main/java/com/ilkgunel/hastaneotomasyonu/controller/SavePatients.java
@@ -8,116 +8,49 @@ package com.ilkgunel.hastaneotomasyonu.controller;
 import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.regex.Matcher;
 import javax.faces.bean.ManagedBean;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import com.ilkgunel.hastaneotomasyonu.entity.Patients;
-import javax.faces.bean.SessionScoped;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import javax.faces.bean.RequestScoped;
+import javax.faces.event.ActionEvent;
 /**
  *
  * @author ilkaygunel
  */
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class SavePatients implements Serializable{
     
     /*@PersistenceContext(unitName = "HospitalAutomation")
     private EntityManager em;*/
+    
+    private Patients patientsObject=new Patients();
 
-    private String identityNumber;
-    private String name;
-    private String surname;
-    private String gender;
-    private String birthplace;
-    private Date birthDate;
-    private String fatherName;
-    private String motherName;
+    public Patients getPatientsObject() throws ParseException{
+        /*String date="01/01/1980";
+        SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
+        patientsObject.setBirthdate(sdf.parse(date));*/
+        return patientsObject;
+    }
+
+    public void setPatientsObject(Patients patientsObject) {
+        this.patientsObject = patientsObject;
+    }
+    
     private String mobilePhoneNumber;
     private String homePhoneNumber;
-    private String emailAddress;
-    private String password;
+    
 
     private String operationResult;
     private Md5PasswordEncoder passwordEncoder;
 
     private EntityManagerFactory emf=Persistence.createEntityManagerFactory("HospitalAutomation");
     private EntityManager em=emf.createEntityManager();
-
-
-    Matcher matcher;
-
-    public String getIdentityNumber() {
-        return identityNumber;
-    }
-
-    public void setIdentityNumber(String identityNumber) {
-        this.identityNumber = identityNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getBirthplace() {
-        return birthplace;
-    }
-
-    public void setBirthplace(String birthplace) {
-        this.birthplace = birthplace;
-    }
-
-    public Date getBirthDate() throws ParseException{
-        String date="01/01/1980";
-        SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
-        birthDate=sdf.parse(date);
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getFatherName() {
-        return fatherName;
-    }
-
-    public void setFatherName(String fatherName) {
-        this.fatherName = fatherName;
-    }
-
-    public String getMotherName() {
-        return motherName;
-    }
-
-    public void setMotherName(String motherName) {
-        this.motherName = motherName;
-    }
 
     public String getMobilePhoneNumber() {
         return mobilePhoneNumber;
@@ -135,21 +68,7 @@ public class SavePatients implements Serializable{
         this.homePhoneNumber = homePhoneNumber;
     }
 
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    
 
     public String getOperationResult() {
         return operationResult;
@@ -159,62 +78,49 @@ public class SavePatients implements Serializable{
         this.operationResult = operationResult;
     }
 
-    public void saveToDb()
+    public void saveToDb(ActionEvent event)
     {
-        Patients PatientObject=new Patients();
-        PatientObject.setIdentitynumber(identityNumber);
-        System.out.println("İsim:"+name);
-        PatientObject.setName(name);
-        System.out.println("Soyİsim:"+surname);
-        PatientObject.setSurname(surname);
-        PatientObject.setGender(gender);
-        System.out.println("Doğum Yeri:"+birthplace);
-        PatientObject.setBirthplace(birthplace);
-
-        java.sql.Date sqlBirDate =new java.sql.Date(birthDate.getTime());
-        PatientObject.setBirthdate(sqlBirDate);
-
-        System.out.println("Baba Adı:"+fatherName);
-        PatientObject.setFathername(fatherName);
-        PatientObject.setMothername(motherName);
-
+        System.err.println("Save To DB Metoduna Girildi!!!!");
         if (mobilePhoneNumber!=null&&homePhoneNumber==null)
         {
-            PatientObject.setPhonenumber(mobilePhoneNumber);
+            patientsObject.setPhonenumber(mobilePhoneNumber);
         }
         else if(mobilePhoneNumber==null&&homePhoneNumber!=null)
         {
-            PatientObject.setPhonenumber(mobilePhoneNumber);
+            patientsObject.setPhonenumber(mobilePhoneNumber);
         }
         else if (mobilePhoneNumber!=null&&homePhoneNumber!=null)
         {
-            PatientObject.setPhonenumber(mobilePhoneNumber);
+            patientsObject.setPhonenumber(mobilePhoneNumber);
         }
-
-        PatientObject.setEmailaddress(emailAddress);
+        
+        System.err.println("Doğum Tarihi"+patientsObject.getBirthdate());
+        java.sql.Date sqlBirthDate =new java.sql.Date(patientsObject.getBirthdate().getTime());
+        patientsObject.setBirthdate(sqlBirthDate);
+        
         try
         {
             MessageDigest messageDigestNesnesi = MessageDigest.getInstance("MD5");
-            messageDigestNesnesi.update(password.getBytes());
+            messageDigestNesnesi.update(patientsObject.getPassword().getBytes());
             byte messageDigestDizisi[] = messageDigestNesnesi.digest();
             StringBuffer sb16 = new StringBuffer();
             StringBuffer sb32 = new StringBuffer();
             for (int i = 0; i < messageDigestDizisi.length; i++)
             {
                 sb16.append(Integer.toString((messageDigestDizisi[i] & 0xff) + 0x100, 16).substring(1));
-                PatientObject.setPassword(sb16.toString());
+                patientsObject.setPassword(sb16.toString());
             }
         }
         catch(NoSuchAlgorithmException ex){
             System.err.println(ex);
         }
 
-        PatientObject.setRole("ROLE_GUEST");
-        PatientObject.setEnabled(true);
+        patientsObject.setRole("ROLE_GUEST");
+        patientsObject.setEnabled(true);
 
         try {
             em.getTransaction().begin();
-            em.persist(PatientObject);
+            em.persist(patientsObject);
             em.getTransaction().commit();
             operationResult="Bilgileriniz Kaydedildi. Sisteme Giriş Yapıp Randevu Alabilirsiniz";
         } catch (Exception e) {
