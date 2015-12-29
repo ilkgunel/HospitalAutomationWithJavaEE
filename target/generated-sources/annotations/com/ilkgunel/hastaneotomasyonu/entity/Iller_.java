e47e9857f012d9872f4aa9225f38afe85fa23aa8
İlkay Günel <ilkgunel93@gmail.com>
2015-12-29 22:47:24 +0200

    IntelliJ Idea Üzerinde WildFly Testi Yapıldı
    IntelliJ IDE üzerinde JBoss WildFly sunucusu kullanılarak proje test
    edildi. Proje üzerinde bazı geliştirmeler planlamaktayım.

--- target/generated-sources/annotations/com/ilkgunel/hastaneotomasyonu/entity/Iller_.java
+++ target/generated-sources/annotations/com/ilkgunel/hastaneotomasyonu/entity/Iller_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-08-12T23:03:52")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-29T22:32:52")
 @StaticMetamodel(Iller.class)
 public class Iller_ { 
 
