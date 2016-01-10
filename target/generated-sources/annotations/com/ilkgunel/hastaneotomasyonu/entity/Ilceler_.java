8bd790698198c2bc885b0ca96d56e7dc00603f55
İlkay Günel <ilkgunel93@gmail.com>
2016-01-10 23:42:52 +0200

    PrimeFaces 5.3'e Güncellendi Ve Captcha Güncellendi
    Oturum açma sayfasındaki Captcha PrimeFaces’ın 5.3’e güncellenmesi ile
    değiştirildi.

--- target/generated-sources/annotations/com/ilkgunel/hastaneotomasyonu/entity/Ilceler_.java
+++ target/generated-sources/annotations/com/ilkgunel/hastaneotomasyonu/entity/Ilceler_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-10T19:13:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-10T23:25:28")
 @StaticMetamodel(Ilceler.class)
 public class Ilceler_ { 
 
